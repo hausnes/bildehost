47da197d76dd633cbf6dbe34db7ab26ee2fecf2c
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-11-12 10:24:34 +0100

    app.js: Går i biletmappa og viser/oppdaterer siste

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+const imagesDir = path.join(__dirname, 'bileter');
+
+// Serverer statiske filer frå biletmappa
+app.use('/bileter', express.static(imagesDir));
+
+// Serverer statiske filer frå public-mappa
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta som viser det siste biletet i mappa
+app.get('/', (req, res) => {
+    fs.readdir(imagesDir, (err, files) => {
+        if (err) {
+            return res.status(500).send('Kunne ikkje scanne mappa');
+        }
+
+        const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
+        if (imageFiles.length === 0) {
+            return res.status(404).send('Fann ingen bileter');
+        }
+
+        const latestImage = imageFiles.sort((a, b) => fs.statSync(path.join(imagesDir, b)).mtime - fs.statSync(path.join(imagesDir, a)).mtime)[0];
+        const latestImagePath = `/bileter/${latestImage}`;
+        const latestImageTime = fs.statSync(path.join(imagesDir, latestImage)).mtime.toLocaleString('no-NO');
+
+        const htmlFilePath = path.join(__dirname, 'public', 'siste.html');
+
+        fs.readFile(htmlFilePath, 'utf8', (err, data) => {
+            if (err) {
+                return res.status(500).send('Fekk ikkje tilgang til HTML-fila');
+            }
+
+            const updatedHtml = data
+                .replace('{{IMAGE_SRC}}', latestImagePath)
+                .replace('{{IMAGE_TIME}}', latestImageTime);
+
+            res.send(updatedHtml);
+        });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
