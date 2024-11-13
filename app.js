188e43c9cbf35ae0c8396d65c335031bb6ae5987
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-11-13 20:00:33 +0100

    Rute til alle bileta, og public-sider som viser
    NB: Dette er per no berre for testing, og er ikkje bekrefta å fungere skikkeleg endå.

--- app.js
+++ app.js
@@ -44,6 +44,37 @@ app.get('/', (req, res) => {
     });
 });
 
+// Ruta som viser alle bileter i mappa
+app.get('/alle', (req, res) => {
+    fs.readdir(imagesDir, (err, files) => {
+        if (err) {
+            return res.status(500).send('Kunne ikkje scanne mappa');
+        }
+
+        const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
+        if (imageFiles.length === 0) {
+            return res.status(404).send('Fann ingen bileter');
+        }
+
+        const imageTags = imageFiles.map(file => {
+            const imagePath = `/bileter/${file}`;
+            return `<div class="image-container"><img src="${imagePath}" alt="${file}"></div>`;
+        }).join('');
+
+        const htmlFilePath = path.join(__dirname, 'public', 'alle.html');
+
+        fs.readFile(htmlFilePath, 'utf8', (err, data) => {
+            if (err) {
+                return res.status(500).send('Fekk ikkje tilgang til HTML-fila');
+            }
+
+            const updatedHtml = data.replace('{{IMAGE_GALLERY}}', imageTags);
+
+            res.send(updatedHtml);
+        });
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
